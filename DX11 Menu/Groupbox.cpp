dda22ca5b112b4711bae94b9ce05f048b71246b0
davidp027 <davidp027@hotmail.com>
2019-02-04 14:33:26 -0500

    Updated gitignore

---
+++ DX11 Menu/Groupbox.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Groupbox.h"
+
+
+Groupbox::Groupbox(Window *parent, Vector2 pos, Vector2 scale) : WindowComponent(parent, pos.x, pos.y, scale.x, scale.y)
+{
+}
+
+
+Groupbox::~Groupbox()
+{
+}
---
+++ DX11 Menu/Groupbox.h
@@ -0,0 +1,8 @@
+#pragma once
+class Groupbox : public WindowComponent
+{
+public:
+	Groupbox(Window *parent, Vector2 pos, Vector2 scale);
+	~Groupbox();
+};
+
