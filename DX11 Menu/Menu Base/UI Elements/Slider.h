49b116a8467607773117ca3d55b4afe2bb173999
David P <davidp027@hotmail.com>
2019-02-05 21:14:56 -0500

    Improved slider drag

--- DX11 Menu/Menu Base/UI Elements/Slider.h
+++ DX11 Menu/Menu Base/UI Elements/Slider.h
@@ -5,12 +5,19 @@ class Slider : public WindowComponent
 	const float LINE_WIDTH = 2.f;
 	const float SLIDER_WIDTH = 8.f;
 	const float SLIDER_HEIGHT = 20.f;
-	POINT m_dragPointBegin = { INT_MAX, INT_MAX };
+
+	DXColor m_mouseOverBox = D3DCOLOR_RGBA(0, 0, 0, 255);
+	DXColor m_defaultBoxColor = D3DCOLOR_RGBA(180, 184, 187, 255);
+	DXColor m_defaultLineColor = D3DCOLOR_RGBA(100, 104, 107, 255);
+	DXColor m_clickedLineColor = D3DCOLOR_RGBA(0, 0, 0, 255);
+
 	float m_sliderPositionOffset = 0;
 
-	int m_value = 50;
-	int m_max = 100;
-	int m_increment = 1;
+	int m_value = 0;
+	int m_max = 500;
+
+	DXColor m_lineColor = m_defaultLineColor;
+	DXColor m_boxColor = m_defaultBoxColor;
 
 	void OnRender() {
 		Vector2 position = GetPosition();
@@ -18,48 +25,35 @@ class Slider : public WindowComponent
 
 		int percent = m_value * 100.f / m_max;
 		m_sliderPositionOffset = percent * size.x / 100.f;
-		DrawRect(position.x, position.y + (size.y / 2.f), size.x, LINE_WIDTH, RED);
-		DrawRect(position.x + m_sliderPositionOffset - (SLIDER_WIDTH / 2.f), position.y, SLIDER_WIDTH, SLIDER_HEIGHT, GREEN);
+		DrawRect(position.x, position.y + (size.y / 2.f), size.x, LINE_WIDTH, m_lineColor);
+		DrawRect(position.x + m_sliderPositionOffset - (SLIDER_WIDTH / 2.f), position.y, SLIDER_WIDTH, SLIDER_HEIGHT, m_boxColor);
 	}
 
-	bool IsMouseOnSlider() {
+	bool IsMouseOnSliderBar() {
 		POINT mousePos = GetMouseRelativePosition();
-		float sliderPosPx = m_sliderPositionOffset + GetParent()->GetWindowPosition().x;
-		return
-			IsMouseOver() &&
-			(mousePos.x > sliderPosPx) &&
-			(mousePos.x < (sliderPosPx + SLIDER_WIDTH));
+		Vector2 position = GetPosition() + GetParent()->GetWindowPosition();
+		Vector2 size = GetSize();
+		if (mousePos.x > position.x && mousePos.x < (position.x + size.x + SLIDER_WIDTH))
+			if (mousePos.y > position.y && mousePos.y < (position.y + size.y))
+				return true;
+		return false;
 	}
-	int count = 1;
+
+
+
 	void OnMouseDown() {
-		printf("MouseDown\n");
-		Vector2 currentPos = GetPosition() + GetParent()->GetWindowPosition();
+		Vector2 sliderBeginPos = GetPosition() + GetParent()->GetWindowPosition();
 		POINT mousePos = GetMouseRelativePosition();
-
-		if (IsMouseOnSlider() && m_dragPointBegin.x == INT_MAX)
-			m_dragPointBegin = mousePos;
-
-		if (m_dragPointBegin.x != INT_MAX) {
-			POINT offset = { mousePos.x - m_dragPointBegin.x, mousePos.y - m_dragPointBegin.y };
-			printf("MousePos.x %i - dragPoint.x - %i\n", mousePos.x, m_dragPointBegin.x);
-			float dragClipDistance = (100.0f / m_max * GetSize().x / 100.0f);
-			printf("%f\nOFF: %i\n", dragClipDistance, offset.x);
-			if (round((offset.x / dragClipDistance)) >= count) {
-				m_value += m_increment;
-				count++;
-			}
-			else if (round((abs(offset.x) / dragClipDistance)) >= count) {
-				m_value -= m_increment;
-				count++;
-			}
+		float sliderEndPos = sliderBeginPos.x + GetSize().x;
+		float dragClipDistance = (100.0f / m_max * GetSize().x / 100.0f);
+		if (IsMouseOnSliderBar()) {
+			int pxOffsetFromStart = mousePos.x - sliderBeginPos.x;
+			if (pxOffsetFromStart <= sliderEndPos)
+				m_value = pxOffsetFromStart / dragClipDistance;
+			
 		}
 	}
 
-	void OnMouseUp() {
-		printf("UP!\n");
-		m_dragPointBegin = { INT_MAX, INT_MAX };
-		count = 0;
-	}
 
 public:
 	Slider(Window* parent, Vector2 pos, float width) : WindowComponent(parent, pos.x, pos.y, 0, 0) { SetSize(Vector2(width, SLIDER_HEIGHT)); }
